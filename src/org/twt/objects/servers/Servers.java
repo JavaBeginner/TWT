b14660e24ac7951e2b18eb115f56d9f02f1e03a5
JavaBeginner <twt.de@gmx.de>
2011-10-06 17:33:39 +0200

    License comment added to the file headers.

--- src/org/twt/objects/servers/Servers.java
+++ src/org/twt/objects/servers/Servers.java
@@ -1,3 +1,19 @@
+/* TWT - Tribal Wars Toolkit is a framework for the game "Die Stämme"
+ * Copyright (C) 2011  JavaBeginner  twt.ds@gmx.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.twt.objects.servers;
 
 import java.io.BufferedReader;
